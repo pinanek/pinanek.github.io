ff2c62649211c3039a2e9974dfb824258cce6d08
pinanek23 <hoangsonngo555@gmail.com>
2022-07-25 01:38:26 +0700

    Added astro-Html-minifier integration

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'astro/config'
 
 import solid from '@astrojs/solid-js'
 import sitemap from '@astrojs/sitemap'
+import astroHtmlMinifier from './integrations/astro-html-minifier'
 
 export default defineConfig({
   site: 'http://pinanek23.pages.dev/',
 
-  integrations: [solid(), sitemap()]
+  integrations: [solid(), sitemap(), astroHtmlMinifier()],
+
+  experimental: {
+    integrations: true
+  }
 })
---
+++ integrations/astro-html-minifier.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as htmlMinifierTerser from 'html-minifier-terser'
+import * as glob from 'fast-glob'
+import type { AstroIntegration } from 'astro'
+
+const baseDir = process.cwd()
+
+function astroHtmlMinifier(): AstroIntegration {
+  return {
+    name: 'astro-Html-minifier',
+    hooks: {
+      'astro:build:done': async function buildDone() {
+        const htmlGlobPaths = glob.sync('dist/**/*.html', { cwd: baseDir })
+
+        const minifiedAll = htmlGlobPaths.map(async (htmlGlobPath) => {
+          const htmlFilePath = path.join(baseDir, htmlGlobPath)
+
+          const htmlFile = await fs.promises.readFile(path.join(baseDir, htmlGlobPath), { encoding: 'utf8' })
+
+          const minifiedHtmlFile = await htmlMinifierTerser.minify(htmlFile, {
+            minifyCSS: true,
+            minifyJS: true,
+            removeComments: true,
+            collapseWhitespace: true
+          })
+
+          await fs.promises.writeFile(htmlFilePath, minifiedHtmlFile, { encoding: 'utf8' })
+        })
+
+        await Promise.all(minifiedAll)
+
+        // eslint-disable-next-line no-console
+        console.log('%sastro-html-minifier:%s %s\n', '\x1b[32m', '\x1b[0m', 'All output html files are minified.')
+      }
+    }
+  }
+}
+
+export default astroHtmlMinifier
