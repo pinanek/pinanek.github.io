68db839f4a3525761b7c37fdd93675c6211ad41e
pinanek23 <hoangsonngo555@gmail.com>
2022-07-27 23:34:27 +0700

    Use `@astrojs/image` 🎉

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,27 @@
 import { defineConfig } from 'astro/config'
 
 import solid from '@astrojs/solid-js'
+import image from '@astrojs/image'
 import sitemap from '@astrojs/sitemap'
 import mdx from '@astrojs/mdx'
-import htmlMinifier from './integrations/astro-html-minifier.mjs'
 import mdxImprovement from './integrations/astro-mdx-improvement.mjs'
+import htmlMinifier from './integrations/astro-html-minifier.mjs'
+
+import remarkMdxImages from 'remark-mdx-images'
 
 export default defineConfig({
   site: 'http://pinanek23.pages.dev/',
 
-  integrations: [solid(), mdx(), mdxImprovement(), sitemap(), htmlMinifier()]
+  integrations: [
+    solid(),
+    image(),
+    mdx({
+      remarkPlugins: {
+        extends: [remarkMdxImages]
+      }
+    }),
+    mdxImprovement(),
+    sitemap(),
+    htmlMinifier()
+  ]
 })
