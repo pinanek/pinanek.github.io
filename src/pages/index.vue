b09f5fe37b64c9be1032719cf32812ddd5d25f9a
pinanek23 <hoangsonngo555@gmail.com>
2022-08-05 18:10:37 +0700

    Update home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,10 @@
 <template layout="home">
   <div class="greeting-container">
-    <p class="hello">Hello, I am</p>
-    <p class="name">Pinanek23</p>
+    <span class="hello">Hello, I am</span>
+    <a href="/about" class="name-link">
+      <span class="name">Pinanek23</span>
+      <span class="indicator"></span>
+    </a>
   </div>
 </template>
 
@@ -24,15 +27,11 @@
   }
 }
 
-.name {
-  font-weight: $font-weight-extra-bold;
-  background: #0466c8;
-  background: linear-gradient(to right top, #0466c8 20%, #a45cba 70%, #a45cba 100%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+.name-link {
+  position: relative;
   font-size: 3rem;
   line-height: calc(1em + 1rem);
+  text-decoration: none;
 
   @media ($breakpoint-sm) {
     font-size: 4rem;
@@ -45,7 +44,31 @@
   }
 }
 
+.name {
+  font-weight: $font-weight-extra-bold;
+  background: #0466c8;
+  background: linear-gradient(to right top, #0466c8 20%, #a45cba 70%, #a45cba 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.indicator {
+  position: absolute;
+  width: 0;
+  left: 0;
+  bottom: 0.075em;
+  height: 0.075em;
+  background: #0466c8;
+  background: linear-gradient(to right top, #0466c8 20%, #a45cba 70%, #a45cba 100%);
+  transition: width $duration-default $ease-in-out;
+
+  .name-link:hover & {
+    width: 100%;
+  }
+}
+
 .name::selection {
-  -webkit-text-fill-color: var(--color-text-inverse);
+  -webkit-text-fill-color: var(--color-text-default);
 }
 </style>
